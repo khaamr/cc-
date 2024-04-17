672a3ec881323baf07bf254cc2a3f8c5ed18c491
khaamr <144602803+khaamr@users.noreply.github.com>
2024-04-17 16:59:40 +0300

    Develop (#3)
    * hell
    
    * hell

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+class Figure {
+private:
+    std::string name;
+public:
+    void setName(std::string n){
+        name=n;
+    }
+    std::string setName(){
+        return name;
+    }
+};
+//
+// Created by Я on 17.04.2024.
+//
+int main (){
+    std::cout<<"hello";
+    return 0;
+};
